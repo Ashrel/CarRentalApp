279cce3717221638aeef871cdd6b925fc34b5f5b
Ashrel <ashrel12345@gmail.com>
2023-07-21 19:27:09 +0800

    ash

--- CarRentalApp/Pages/Cars/Index.cshtml.cs
+++ CarRentalApp/Pages/Cars/Index.cshtml.cs
@@ -4,15 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.Extensions.Configuration;
 namespace CarRentalApp.Pages.Cars
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
         public string Heading { get; set; }
+        public string Message { get; set; }
         public void OnGet()
         {
             Heading = "List of Cars";
+            Message = _configuration["Message"];
         }
     }
 }

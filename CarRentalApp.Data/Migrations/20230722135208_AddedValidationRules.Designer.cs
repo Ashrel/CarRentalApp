53880b191db5f9626729e1eb2b47ff9c819ee3cf
Teo Keeman <s10247202@connect.np.edu.sg>
2023-07-22 21:58:32 +0800

    added create page

--- CarRentalApp.Data/Migrations/20230722135208_AddedValidationRules.Designer.cs
+++ CarRentalApp.Data/Migrations/20230722135208_AddedValidationRules.Designer.cs
@@ -3,14 +3,16 @@ using CarRentalApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarRentalApp.Data.Migrations
 {
     [DbContext(typeof(CarRentalAppDbContext))]
-    partial class CarRentalAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230722135208_AddedValidationRules")]
+    partial class AddedValidationRules
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

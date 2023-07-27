e8c80dbe1e224b38751849331b366b92abf3de54
Ashrel <ashrel12345@gmail.com>
2023-07-28 00:30:25 +0800

    added cascading sliders for update and create

---
+++ CarRentalApp/wwwroot/js/CascadingModelsDropdownListScript.js
@@ -0,0 +1,17 @@
+﻿
+var makeId = $('makes').val();
+
+$('#makes').change(function () {
+    makeId = $(this).val();
+    $('#models').html('<option>Select Model<\option>');
+    LoadModels();
+    
+});
+function LoadModels() {
+    $.getJSON('/Cars/Create?handler=CarModels', { makeId: makeId }, function (date) {
+        $.each(data, function (key, value) {
+            var option = $('<option><\option>').attr('value', value.id).text(value.name);
+            $('#models').append(option);
+        });
+    });
+}

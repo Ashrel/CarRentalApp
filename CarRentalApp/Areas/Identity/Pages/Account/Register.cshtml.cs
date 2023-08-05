909e80d499f0ab6abf157a86fdcf03709cd3c8ec
Ashrel <ashrel12345@gmail.com>
2023-08-05 16:08:51 +0800

    added code to add users automatically to user roles

--- CarRentalApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CarRentalApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -88,23 +88,15 @@ namespace CarRentalApp.Areas.Identity.Pages.Account
             {
                 var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName,
                     LastName = Input.LastName, DateOfBirth = Input.DateOfBirth
-                };
+                };               
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
-                   // var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                   // code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    //var callbackUrl = Url.Page(
-                     //   "/Account/ConfirmEmail",
-                      //  pageHandler: null,
-                       // values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                       // protocol: Request.Scheme);
-
-                   // await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                   //     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
+                    
+                    //adds registered users automatically to user roles
+                    var roleResult = await _userManager.AddToRoleAsync(user, "Users");
+                    
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
